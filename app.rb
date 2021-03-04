dd9cca09c7dc37059ce0402e4e056faf8134455b
noah <crashlive112@gmail.com>
2021-03-03 19:14:21 -0600

    commit tree bfd2eef4ad4f765a2f79553931e1bad2596ee5ce

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/' do
+    erb :user_input
+  end
+
+  post '/piglatinize' do
+    pl = PigLatinizer.new #creates new instance of the PigLatinizer class
+    @piglatin = pl.piglatinize(params[:user_phrase]) #sets instance variable equal to users input
+
+    erb :results #displays results.erb on user input
+  end
 end
